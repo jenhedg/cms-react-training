7f50a883d18ab8ec877b943bbb4090e6334c1940
Jennifer Hedgcock <jennifer.hedgcock@simpleviewinc.com>
2023-02-10 12:46:38 -0700

    setup app, create comicList, comic components

---
+++ cms-react-training-app/pages/Comic.tsx
@@ -0,0 +1,9 @@
+export default function Comic() {
+    return (
+        <div>
+            <li>I am a comic</li>
+            <li>I am a comic 2</li>
+            <li>I am a comic 3</li>
+        </div>
+    )
+}
---
+++ cms-react-training-app/pages/ComicsList.tsx
@@ -0,0 +1,11 @@
+import Comic from "./Comic"
+
+export default function ComicsList() {
+    return (
+        <div>
+            <ul className="comics-list">
+                <Comic></Comic>
+            </ul>
+        </div>
+    )
+}
