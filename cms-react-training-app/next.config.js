6e3319b8315ca2836720128fab177654e6c4de1f
Jennifer Hedgcock <jennifer.hedgcock@simpleviewinc.com>
2023-03-05 20:57:01 -0700

    construct api url - wip

--- cms-react-training-app/next.config.js
+++ cms-react-training-app/next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
   images: {
     domains: ["i.annihil.us"],
   },
+  env: {
+		apiKeyPublic: "0cd95d1798ab43b28846186ed5f58e07",
+		apiKeyPrivate: "61dffe69ef2a60a0d40b1e389f9e52b62642bce9",
+	},
 }
 
 module.exports = nextConfig
